54e8559ff3f23d6effa8ae6d93308e8e8cad4cc8
Tomas Berg <tomas.berg+bitbucket@gmail.com>
2011-11-11 06:39:56 +0100

    DialogManager test

---
+++ PunchClock/src/se/kingharvest/infrastructure/property/BooleanProperty.java
@@ -0,0 +1,12 @@
+package se.kingharvest.infrastructure.property;
+
+
+public class BooleanProperty extends Property<Boolean>{
+
+	public BooleanProperty(boolean b) {
+		set(b);
+	}
+
+	private static final long serialVersionUID = 5554241819656353305L;
+
+}
--- PunchClock/src/se/kingharvest/punchclock/PunchClockActivity.java
+++ PunchClock/src/se/kingharvest/punchclock/PunchClockActivity.java
@@ -2,6 +2,7 @@ package se.kingharvest.punchclock;
 
 import se.kingharvest.infrastructure.ui.ActivityBase;
 import se.kingharvest.infrastructure.ui.DialogBase;
+import se.kingharvest.infrastructure.ui.DialogManager;
 import se.kingharvest.infrastructure.ui.annotation.OnActivityResult;
 import se.kingharvest.infrastructure.ui.annotation.OnClick;
 import se.kingharvest.punchclock.pages.EntriesPage;
@@ -30,6 +31,9 @@ public class PunchClockActivity extends ActivityBase<PunchClockActivity, PunchCl
 
         getButton(R.id.punchclock_go_to_entries)
         	.setOnClick(onClick(R.id.punchclock_go_to_entries));
+
+        getButton(R.id.punchclock_show_dialog_button)
+        	.setOnClick(onClick(R.id.punchclock_show_dialog_button));
     }
 
 	public void bindViewModel() 
@@ -49,8 +53,48 @@ public class PunchClockActivity extends ActivityBase<PunchClockActivity, PunchCl
 		Dialog d = new DialogBase(this);
 		d.setTitle("Hello");
 		d.show();
+		
+		getTextView(R.id.punchclock_just_text).setText("This happens while the dialog shows.");
 	}
 
+	Dialog a;
+	Dialog b;
+	Dialog c;
+	
+	@OnClick(R.id.punchclock_show_dialog_button)
+	public void showDialog(View v)
+	{
+		a = new DialogBase(this);
+		a.setTitle("A!");
+
+		b = new DialogBase(this);
+		b.setTitle("B!");
+
+		c = new DialogBase(this);
+		c.setTitle("C!");
+
+		new Thread(new Runnable() {			
+			public void run() {
+				dialogWorkflow();
+			}
+		}).start();
+	}
+	
+	private void dialogWorkflow()
+	{
+		DialogManager.show(this, a);
+
+		//getTextView(R.id.punchclock_just_text).setText("A done.");
+
+		DialogManager.show(this, b);
+
+		//getTextView(R.id.punchclock_just_text).setText("B done.");
+
+		DialogManager.show(this, c);
+
+		//getTextView(R.id.punchclock_just_text).setText("C done.");
+	}
+	
 	@OnClick(R.id.punchclock_go_to_entries)
 	public void goToEntries(View button)
 	{
